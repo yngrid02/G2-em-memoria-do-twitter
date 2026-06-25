02b637b7ed213c6d5b1b91fe192de95a7abdd8cf
yngrid <franciscocasimirojunior16@gmail.com>
2026-06-25 20:55:25 -0300

    commit tela login e cadastro

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
 
-function App(){
-
-return(
-
-<Cadastro/>
-
-);
-
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
